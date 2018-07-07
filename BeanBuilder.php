e3237277c25a3302bda40c728c429120e76356f9
a_vrb <antonio_vrb@hotmail.com>
2018-07-07 12:41:25 +0200

    initial commit

---
+++ BeanBuilder.php
@@ -0,0 +1,63 @@
+<?php
+
+abstract class BeanBuilder {
+    /**
+     * @var SugarBean $bean object to create
+     */
+    protected $bean = NULL;
+    
+    /**
+     * Constructor
+     *
+     * Initializes new SugarBean object
+     *
+     * @return void
+     */
+    protected function __construct(String $moduleName) {
+        $this->$bean = BeanFactory::newBean($moduleName);
+    }
+    
+    /**
+     * Sets name for SugarBean
+     *
+     * @param string $name bean name
+     * @return BeanBuilder
+     */
+    function name($name){
+        $this->bean->name = $name;
+        return $this;
+    }
+    
+    
+    /**
+     * Sets description for SugarBean
+     *
+     * @param string $description bean description
+     * @return BeanBuilder
+     */
+    function description($description = ''){
+        $this->bean->description = $description;
+        return $this;
+    }
+    
+    /**
+     * Sets id of Assigned User for SugarBean
+     *
+     * @param string $userId ID of Users bean
+     * @return BeanBuilder
+     */
+    function assignedUser($userId = 1){
+        $this->bean->assigned_user_id = $userId;
+        return $this;
+    }
+    
+    /**
+     * Returns SugarBean object
+     *
+     * @return SugarBean
+     */
+    function build(){
+        return $this->bean;
+    }
+}
+?>
