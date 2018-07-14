22899efce66aed7055c04a7db75a8f57b46dc272
a_vrb <antonio_vrb@hotmail.com>
2018-07-14 18:31:54 +0200

    Added CurrenciesBuilder

---
+++ src/builders/CurrenciesBuilder.php
@@ -0,0 +1,51 @@
+<?php
+require_once 'BeanBuilder.php';
+
+/**
+ *  Class CurrenciesBuilder
+ *
+ * Builder class for creating new Currency beans. 
+ * Class only creates new instance of an object, to persist this object one must call SugarBean::save() function.
+ *
+ * @package Custom/Currencies/Builder
+ * @author Antonio Jedvaj <antonio_vrb@hotmail.com>
+ */
+class CurrenciesBuilder extends BeanBuilder
+{
+    const STATUS_ACTIVE = "Active";
+    const STATUS_INACTIVE = "Inactive";
+
+    /**
+     * Constructor
+     *
+     * Initializes new Currencies bean
+     *
+     * @return void
+     */
+    function __construct()
+    {
+        parent::__construct('Currencies');
+    }
+
+    function iso4217($iso4217){
+        $this->bean->iso4217 = $iso4217;
+        return $this;
+    }
+
+    function conversionRate($conversionRate){
+        $this->bean->conversion_rate = $conversionRate;
+        return $this;
+    }
+    
+    function symbol($symbol){
+        $this->bean->symbol = $symbol;
+        return $this;
+    }
+    
+    function status($status){
+        $this->bean->status = $status;
+        return $this;
+    }
+}
+
+?>
